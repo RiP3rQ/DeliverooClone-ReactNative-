39bc06eb4eef828100528bec1be5ef08ce612215
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-12 20:46:29 +0100

    feat: setting up react-navigation

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+
+const Stack = createNativeStackNavigatoravigator();
 
 export default function App() {
   return (
-    <View>
-      <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          {/* SCREENS */}
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -3,6 +3,8 @@ module.exports = {
         "./screens/**/*.{js,jsx,ts,tsx}", 
         "./pages/**/*.{js,jsx,ts,tsx}",
         "./components/**/*{js,jsx,ts,tsx}",
+        "./App.{js,jsx,ts,tsx}", 
+        "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
        extend: {},
