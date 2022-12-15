f1eb0793c143a25001f9051ca86ee14ecbfff037
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-15 20:30:24 +0100

    feat: dishRows component

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,7 +2,9 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity'
-import { ArrowLeftIcon, StarIcon } from 'react-native-heroicons/solid'
+import { ArrowLeftIcon, ChevronRightIcon, StarIcon } from 'react-native-heroicons/solid'
+import { MapPinIcon, QuestionMarkCircleIcon } from 'react-native-heroicons/outline'
+import DishRow from '../components/DishRow'
 
 const RestaurantScreen = () => {
     const navigation = useNavigation()
@@ -30,6 +32,7 @@ const RestaurantScreen = () => {
   
     return (
     <ScrollView>
+        {/* header elements like background and comeback button */}
         <View className="relative">
             <Image 
                 source={{
@@ -45,26 +48,56 @@ const RestaurantScreen = () => {
         </View>
 
         <View className="bg-white">
+            {/* top elements like name, rating etc. */}
             <View className="px-4 pt-4">
                 <Text className="text-3xl font-bold">{title}</Text>
                 <View className="flex-row space-x-2 my-1">
-                    // rating
+                     {/* rating */}
                     <View className='flex-row items-center space-x-1'>
                         <StarIcon color='green' opacity={0.5} size={22}/>
                         <Text className='text-xs text-gray-500'>
                             <Text className='text-green-500'>{rating}</Text>
                         </Text>
                     </View>
-                    // location
+                    {/* location */}
                     <View className='flex-row items-center space-x-1'>
-                        <StarIcon color='green' opacity={0.5} size={22}/>
+                        <MapPinIcon color='gray' opacity={0.4} size={22}/>
                         <Text className='text-xs text-gray-500'>
-                            <Text className='text-green-500'>{rating}</Text>
+                            <Text className='text-gray-500'>Nearby • {address}</Text>
                         </Text>
                     </View>
                 </View>
+
+                <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
             </View>
+
+                {/* allergies */}
+            <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+                <QuestionMarkCircleIcon color='gray' opacity={0.6} size={20}/>
+                <Text className="pl-2 flex-1 text-md font-bold">
+                    Have a food allergy?
+                </Text>
+                <ChevronRightIcon color='#00CCBB'/>
+            </TouchableOpacity>
         </View>
+        {/* end of first half of the screen */}
+
+        
+        <View>
+            <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+            {/* Dishrows */}
+            {dishes.map((dish) => (
+                <DishRow 
+                key={dish._id}
+                id={dish._id}
+                name={dish.name}
+                description={dish.short_description}
+                price={dish.price}
+                image={dish.image}
+                />
+            ))}
+        </View>
+
     </ScrollView>
   )
 }
